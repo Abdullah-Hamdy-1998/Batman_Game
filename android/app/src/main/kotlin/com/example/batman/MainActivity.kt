c97474fe0768198cde4134527eff753c955f81a1
Abdullah Hamdy <Obaida.hamdy.1998@gmail.com>
2022-05-28 05:14:34 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/batman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.batman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GameApp());
+}
+
+class GameApp extends StatelessWidget {
+  const GameApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
